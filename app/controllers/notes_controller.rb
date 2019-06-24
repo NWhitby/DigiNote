a5523d14f6e88f4cb5d228566108b77cfa41531e
NaKia Whitby <whitby.nakia@gmail.com>
2019-06-24 17:24:48 +0000

    Add get and post notes routes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,34 @@
 class NotesController < ApplicationController
+
+  get '/notes' do
+   redirect_if_not_logged_in
+   @notes = Note.all
+   erb :'/notes/notes'
+ end
+
+ get '/notes/new' do
+   redirect_if_not_logged_in
+   erb :'/notes/new'
+ end
+
+ post '/notes' do
+   if logged_in?
+     if params["note"]["title"] == "" || params["note"]["content"] == ""       
+       redirect to '/notes/new'
+     else
+       @user = current_user
+       @note = Note.create(title: params["note"]["title"], content: params["note"]["content"], user_id: session[:user_id])
+       if @note.save
+         @user.notes << @note
+         redirect to '/notes'
+       else
+         redirect to '/notes/new'
+       end
+     end
+   else
+     redirect to '/login'
+   end
+ end
+
+
 end
