4c5b829a7765d7519c602a46d0f2a38f27e84305
NaKia Whitby <whitby.nakia@gmail.com>
2019-06-24 17:28:43 +0000

    Add notes patch/edit routes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,7 +13,7 @@ class NotesController < ApplicationController
 
  post '/notes' do
    if logged_in?
-     if params["note"]["title"] == "" || params["note"]["content"] == ""       
+     if params["note"]["title"] == "" || params["note"]["content"] == ""
        redirect to '/notes/new'
      else
        @user = current_user
@@ -29,6 +29,34 @@ class NotesController < ApplicationController
      redirect to '/login'
    end
  end
+
+ get '/notes/:id/edit' do
+   redirect_if_not_logged_in
+   @note = Note.find_by(id: params[:id])
+   if @note && @note.user == current_user
+     erb :'/notes/edit'
+   else
+     redirect to '/notes'
+   end
+ end
+
+ patch '/notes/:id' do
+ redirect_if_not_logged_in
+   if params["note"]["title"] == "" || params["note"]["content"] == ""
+     redirect to '/notes/:id/edit'
+   else
+     @note = Note.find_by(id: params[:id])
+     if @note && @note.user == current_user
+       if @note.update(title: params["note"]["title"], content: params["note"]["content"])         
+         redirect to '/notes'
+       else
+         redirect to '/notes'
+       end
+     end
+   end
+ end
+
+
 
 
 end
