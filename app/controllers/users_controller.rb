9b10aef49bf5db79d1c4b13e9f8931813b5ffc07
NaKia Whitby <whitby.nakia@gmail.com>
2019-06-24 16:36:50 +0000

    Add get/post login routes to users/add login template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+ get '/login' do
+   if !logged_in?
+     erb :'/users/login'
+   else
+     redirect to '/notes'
+   end
+ end
+
+ post '/login' do
+   user = User.find_by(username: params[:username])
+   if user && user.authenticate(params[:password])
+     session[:user_id] = user.id
+     redirect to '/notes'
+   else
+     redirect to '/login'
+   end
+ end
+
+
 end
