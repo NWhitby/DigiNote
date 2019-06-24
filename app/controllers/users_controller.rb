32bd7fdf8621adb3c9bb3bee80eebd8e98f1412f
NaKia Whitby <whitby.nakia@gmail.com>
2019-06-24 16:55:28 +0000

    Add get and post signup routes/add signup template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+  if !logged_in?
+    erb :'/users/signup'
+  else
+    redirect to '/notes'
+  end
+ end
+
+ post '/signup' do
+   if params[:username] == "" || params[:email] == "" || params[:password] == ""
+     redirect to '/signup'
+   elsif User.all.find {|user| user.username == params[:username]}
+     redirect to '/signup'
+   elsif User.all.find {|user| user.email == params[:email]}
+     redirect to '/signup'
+   else
+     @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+     @user.save
+     session[:user_id] = @user.id
+     redirect to '/notes'
+   end
+ end 
+
  get '/login' do
    if !logged_in?
      erb :'/users/login'
